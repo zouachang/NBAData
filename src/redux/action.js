7f745e4429c0c8bde482f8bb66f7f38a04d014f7
邹永昌 <zouachang@126.com>
2019-11-26 17:47:07 +0800

    Test

--- src/redux/action.js
+++ src/redux/action.js
@@ -4,8 +4,12 @@ import axios from 'axios';
 export const initTeamData = () => {
     return dispatch => {
         return (
-            axios.get('http://wc.miguvideo.com/vms-worldcup/competition-rank/team-scoreboard/2221402/REG')
-                .then(
+
+            axios.get('/vms-worldcup/competition-rank/team-scoreboard/2221402/REG', {
+                headers: {
+                    'Access-Control-Allow-Origin': '*'
+                }
+            }).then(
                     (res) => {
                         if (res.data.code === 200 && res.data.body && res.data.body.standings) {
                             dispatch(initTeamDataAction(res.data.body.standings));
@@ -22,7 +26,7 @@ export const initTeamData = () => {
 export const initPlayerData = () => {
     return dispatch => {
         return (
-            axios.get('http://wc.miguvideo.com/vms-worldcup/competition-rank/player-season-rank/2/REG/2221402/2/1')
+            axios.get('/vms-worldcup/competition-rank/player-season-rank/2/REG/2221402/2/1')
                 .then(
                     (res) => {
                         if (res.data.code === 200 && res.data.body && res.data.body) {
